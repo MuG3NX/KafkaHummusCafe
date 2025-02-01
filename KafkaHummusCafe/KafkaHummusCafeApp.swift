780902fe22f67f5083d665e886e4c2ab3c353bdd
alessandrobryk@gmail.com <alessandrobryk@gmail.com>
2025-02-01 15:22:05 +0100

    Initial Commit

---
+++ KafkaHummusCafe/KafkaHummusCafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KafkaHummusCafeApp.swift
+//  KafkaHummusCafe
+//
+//  Created by Alessandro Bryk on 01.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct KafkaHummusCafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
